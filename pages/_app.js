53d71f0ed62d3471f526758d7e332338fcd6d840
Ivaylo Aleksandrov <44462495+SkillControlWinsdom@users.noreply.github.com>
2024-04-28 17:35:30 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-import { ThemeProvider } from "styled-components";
-import { theme } from "~/styles";
+// pages/_app.js
+
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './globalStyles'; // Use a relative path to import globalStyles.js
+import { theme } from '~/styles';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles /> {/* Include GlobalStyles component */}
       <Component {...pageProps} />
     </ThemeProvider>
   );
